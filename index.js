fb09612d3e4137486c652a328888872eba5c1548
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-24 02:24:57 -0400

    lint

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * for-in <https://github.com/jonschlinkert/for-in>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT License
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
