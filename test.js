567e65b0f0edda94910834526a080726947be167
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-27 10:54:58 -0400

    lint, run `update`

--- test.js
+++ test.js
@@ -1,22 +1,16 @@
-/*!
- * for-in <https://github.com/jonschlinkert/for-in>
- *
- * Copyright (c) 2014-2015, Jon Schlinkert.
- * Licensed under the MIT License.
- */
-
 'use strict';
 
+require('mocha');
 var should = require('should');
 var forIn = require('./');
 
-describe('.forIn()', function () {
-  it('should loop through all properties in the object.', function () {
+describe('.forIn()', function() {
+  it('should loop through all properties in the object.', function() {
     var obj = {a: 'foo', b: 'bar', c: 'baz'};
     var values = [];
     var keys = [];
 
-    forIn(obj, function (value, key, o) {
+    forIn(obj, function(value, key, o) {
       o.should.eql(obj);
       keys.push(key);
       values.push(value);
@@ -26,12 +20,12 @@ describe('.forIn()', function () {
     values.should.eql(['foo', 'bar', 'baz']);
   });
 
-  it('should break the loop early if `false` is returned.', function () {
+  it('should break the loop early if `false` is returned.', function() {
     var obj = {a: 'foo', b: 'bar', c: 'baz'};
     var values = [];
     var keys = [];
 
-    forIn(obj, function (value, key, o) {
+    forIn(obj, function(value, key, o) {
       if (key === 'b') {
         return false;
       }
